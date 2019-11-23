277ef7cc39eabb7f12dde7d29127ff49ada1847e
lhmiao <2696254238@qq.com>
2019-11-23 23:18:40 +0800

    feat: 添加 isUserExist service

--- app/service/user.js
+++ app/service/user.js
@@ -12,12 +12,13 @@ module.exports = class UserService extends Service {
   }
 
   updateUser(userInfo) {
-    const { id } = userInfo;
-    return this.ctx.model.User.update(userInfo, { where: { id } });
+    const where = { id: userInfo.id };
+    return this.ctx.model.User.update(userInfo, { where });
   }
 
   async checkUserInfo({ username, password }) {
-    const record = await this.ctx.model.User.findOne({ where: { username } });
+    const where = { username };
+    const record = await this.ctx.model.User.findOne({ where });
     if (!record) return Promise.reject({ name: '该用户不存在' }); // eslint-disable-line
     if (record.password !== password) return Promise.reject({ name: '密码错误' }); // eslint-disable-line
     return Promise.resolve(record);
@@ -26,5 +27,11 @@ module.exports = class UserService extends Service {
   clearLoginCookieAndSession(id) {
     this.ctx.cookies.set('id', '', { maxAge: 0 });
     this.ctx.session[`loginUser-${id}`] = null;
+  }
+
+  async isUserIdExist(id) {
+    const record = await this.ctx.model.User.findById(id);
+    if (record) return Promise.resolve(true);
+    return Promise.reject({ name: `不存在 id 为"${id}"的用户` }); // eslint-disable-line
   }
 };
