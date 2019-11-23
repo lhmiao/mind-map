5617b5bd9ce260ac0d5ac9d229522557ec038753
lhmiao <2696254238@qq.com>
2019-11-24 01:32:12 +0800

    fix: 未匹配的请求直接放过

--- app/middleware/processResBody.js
+++ app/middleware/processResBody.js
@@ -2,6 +2,7 @@ const { SUCCESS_CODE, RES_BODY_KEYS } = require('../constant');
 
 module.exports = () => async function processResBody(ctx, next) {
   await next();
+  if (ctx.body === undefined) return; // 未匹配的请求直接放过
   const keys = Object.keys(ctx.body);
   const needProcess = !RES_BODY_KEYS.every(key => keys.includes(key));
   if (needProcess) {
