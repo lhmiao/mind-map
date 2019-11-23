35e95316041e265ded64dfb5f7212139c2c36cb7
lhmiao <2696254238@qq.com>
2019-11-23 23:19:49 +0800

    refactor: friend 接口路径重构

--- app/router/friend.js
+++ app/router/friend.js
@@ -1,7 +1,9 @@
 module.exports = app => {
   const { router, controller } = app;
   router.get('/friend', controller.friend.getFriendList);
-  router.delete('/friend', controller.friend.deleteFriend);
+  router.delete('/friend/:friend_id', controller.friend.deleteFriend);
+  router.get('/friend/apply', controller.friend.getApplyList);
   router.post('/friend/apply', controller.friend.applyFriend);
   router.post('/friend/agree', controller.friend.agreeFriendApply);
+  router.delete('/friend/agree/:friend_id', controller.friend.disagreeFriendApply);
 };
