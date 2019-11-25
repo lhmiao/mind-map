2126bdf124b722a5d14eeb728bef588f9a3b35dc
lhmiao <2696254238@qq.com>
2019-11-25 22:22:15 +0800

    feat: 添加未授权状态码

--- app/constant.js
+++ app/constant.js
@@ -6,6 +6,8 @@ exports.ERROR_CODE = 1;
 
 exports.NEED_LOGIN_CODE = 2;
 
+exports.NO_AUTH_CODE = 3;
+
 exports.RES_BODY_KEYS = ['code', 'message', 'data'];
 
 exports.LOGIN_COOKIE_MAX_AGE = ms('7d');
