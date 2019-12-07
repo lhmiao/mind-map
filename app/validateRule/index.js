92ad7bf9c0cc9d50fbce08d2963cd02f18ee31d2
lhmiao <2696254238@qq.com>
2019-12-07 23:30:27 +0800

    fix: 修复校验规则对象初始化不全的问题

--- app/validateRule/index.js
+++ app/validateRule/index.js
@@ -1,14 +1,22 @@
 const fs = require('fs');
 const path = require('path');
 
-const ruleObject = {};
+const ruleObject = {
+  get: {},
+  post: {},
+  put: {},
+  patch: {},
+  delete: {},
+};
 const ruleFilesName = fs
   .readdirSync(__dirname)
   .filter(fileName => fileName !== 'index.js');
 ruleFilesName.forEach(fileName => {
   const filePath = path.resolve(__dirname, fileName);
   const fileModule = require(filePath);
-  Object.assign(ruleObject, fileModule);
+  Object
+    .entries(fileModule)
+    .forEach(([method, rule]) => Object.assign(ruleObject[method], rule));
 });
 
 module.exports = ruleObject;
